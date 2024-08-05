17184814bde5759df6cb8b373ba83a5d72eaaa22
yash5676 <yashkumargoyal1804@gmail.com>
2024-08-05 07:25:58 +0530

    frontend completed

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { AuthProvider } from "./context/AuthProvider.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { SocketProvider } from "./context/SocketContext.jsx";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <AuthProvider>
+      <SocketProvider>
+        <App />
+      </SocketProvider>
+    </AuthProvider>
+  </BrowserRouter>
+);
