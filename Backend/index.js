dfc7ce6b98596b5ec3f6c4b4e1c1103a95be150e
yash5676 <yashkumargoyal1804@gmail.com>
2024-08-05 07:40:56 +0530

    updated index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,34 @@
-const express = require('express')
-const app = express()
-const port =4001;
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
-app.get('/',(req, res) =>{
-  res.send('Hello World');
-})
+import userRoute from "./routes/user.route.js";
+import messageRoute from "./routes/message.route.js";
+import { app, server } from "./SocketIO/server.js";
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+dotenv.config();
+
+// middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+const PORT = process.env.PORT || 3001;
+const URI = process.env.MONGODB_URI;
+
+try {
+    mongoose.connect(URI);
+    console.log("Connected to MongoDB");
+} catch (error) {
+    console.log(error);
+}
+
+//routes
+app.use("/api/user", userRoute);
+app.use("/api/message", messageRoute);
+
+server.listen(PORT, () => {
+    console.log(`Server is Running on port ${PORT}`);
 });
